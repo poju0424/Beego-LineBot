a4ff67aa882830c41c1469a4c44bd3e179a2a730
pojulee <poju0424@gmail.com>
2017-05-10 11:28:12 +0800

    123

--- routers/router.go
+++ routers/router.go
@@ -9,10 +9,10 @@ import (
 )
 
 func init() {
-	beego.Router("/", &controllers.MainController{})
+	beego.Router("/123", &controllers.MainController{})
 	beego.Handler("/callback", &models.LineHandler{})
-	
-	beego.Handler("/currency/?:name/?:time", &models.LineHandler{})	//monthly
-	beego.Handler("/currency/?:name/ltm", &models.LineHandler{})	//last three month
-	beego.Handler("/currency/?:name/l6m", &models.LineHandler{})	//last half year
+
+	beego.Handler("/currency/?:name/?:time", &models.LineHandler{}) //monthly
+	beego.Handler("/currency/?:name/ltm", &models.LineHandler{})    //last three month
+	beego.Handler("/currency/?:name/l6m", &models.LineHandler{})    //last half year
 }
