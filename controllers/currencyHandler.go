529f7f92d4a7395e02b624270129b1209e7f6117
pojulee <poju0424@gmail.com>
2017-05-15 16:17:22 +0800

    test

--- controllers/currencyHandler.go
+++ controllers/currencyHandler.go
@@ -67,15 +67,17 @@ func getData(time, name string) *RateHistoryStruct {
 		log.Print(err)
 	}
 	doc.Find("tbody").Find("tr").Each(func(i int, s *goquery.Selection) {
+		date := s.Find("td").Eq(0).Text()
 		cashBuy, _ := strconv.ParseFloat(s.Find("td").Eq(2).Text(), 64)
 		cashSell, _ := strconv.ParseFloat(s.Find("td").Eq(3).Text(), 64)
 		rateBuy, _ := strconv.ParseFloat(s.Find("td").Eq(4).Text(), 64)
 		rateSell, _ := strconv.ParseFloat(s.Find("td").Eq(5).Text(), 64)
 
 		history.CashBuy = append(history.CashBuy, cashBuy)
-		history.CashSell = append(history.CashBuy, cashSell)
-		history.RateBuy = append(history.CashBuy, rateBuy)
-		history.RateSell = append(history.CashBuy, rateSell)
+		history.CashSell = append(history.CashSell, cashSell)
+		history.RateBuy = append(history.RateBuy, rateBuy)
+		history.RateSell = append(history.RateSell, rateSell)
+		history.Date = append(history.Date, date)
 		// perHistory := PerHistory{
 		// 	date:     s.Find("td").Eq(0).Text(),
 		// 	cashBuy:  cashBuy,
