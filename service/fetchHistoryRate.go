93f22619ca8ab36b78ca9d715da1275fee30f73f
pojulee <poju0424@gmail.com>
2017-06-08 17:19:51 +0800

    abc

---
+++ service/fetchHistoryRate.go
@@ -0,0 +1,37 @@
+package service
+
+// type RateHistoryStruct struct {
+// 	CashBuy      []float64
+// 	CashSell     []float64
+// 	RateBuy      []float64
+// 	RateSell     []float64
+// 	Date         []time.Time
+// 	CurrencyName string
+// }
+
+// func getData(date, name string) *RateHistoryStruct {
+// 	url := "http://rate.bot.com.tw/xrt/quote/" + date + "/" + name + ""
+// 	doc, err := goquery.NewDocument(url)
+// 	history := NewRateHistoryStruct(name)
+
+// 	if err != nil {
+// 		log.Print(err)
+// 	}
+// 	doc.Find("tbody").Find("tr").Each(func(i int, s *goquery.Selection) {
+// 		date := s.Find("td").Eq(0).Text()
+// 		date1, _ := time.Parse("2006/01/02", date)
+
+// 		cashBuy, _ := strconv.ParseFloat(s.Find("td").Eq(2).Text(), 64)
+// 		cashSell, _ := strconv.ParseFloat(s.Find("td").Eq(3).Text(), 64)
+// 		rateBuy, _ := strconv.ParseFloat(s.Find("td").Eq(4).Text(), 64)
+// 		rateSell, _ := strconv.ParseFloat(s.Find("td").Eq(5).Text(), 64)
+
+// 		history.CashBuy = append(history.CashBuy, cashBuy)
+// 		history.CashSell = append(history.CashSell, cashSell)
+// 		history.RateBuy = append(history.RateBuy, rateBuy)
+// 		history.RateSell = append(history.RateSell, rateSell)
+// 		history.Date = append(history.Date, date1)
+
+// 	})
+// 	return history
+// }
