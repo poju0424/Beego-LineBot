b3940f86f4cd11585a226fe9b57a032378d932b6
pojulee <poju0424@gmail.com>
2017-06-09 10:31:32 +0800

    test

--- controllers/currencyHandler.go
+++ controllers/currencyHandler.go
@@ -1,37 +1,25 @@
 package controllers
 
 import (
+	"Beego-LineBot/service"
 	"bytes"
 	"log"
 	"net/http"
 	"strconv"
 	"strings"
 
-	"time"
-
-	"github.com/PuerkitoBio/goquery"
 	"github.com/shopspring/decimal"
 	chart "github.com/wcharczuk/go-chart"
 )
 
 type CurrencyHandler struct{}
 
-type RateHistoryStruct struct {
-	CashBuy      []float64
-	CashSell     []float64
-	RateBuy      []float64
-	RateSell     []float64
-	Date         []time.Time
-	CurrencyName string
-}
-
 func (*CurrencyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
 	params := strings.Split(r.RequestURI, "/")
 	if len(params) == 4 {
 		time := params[2]
 		name := params[3]
-		data := getData(time, name)
+		data := service.GetData(time, name)
 		buff := createChart(data)
 		w.Header().Set("Content-Type", "image/jpeg")
 		w.Header().Set("Content-Length", strconv.Itoa(len(buff.Bytes())))
@@ -43,40 +31,7 @@ func (*CurrencyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func NewRateHistoryStruct(name string) *RateHistoryStruct {
-	obj := new(RateHistoryStruct)
-	obj.CurrencyName = name
-	return obj
-}
-
-func getData(date, name string) *RateHistoryStruct {
-	url := "http://rate.bot.com.tw/xrt/quote/" + date + "/" + name + ""
-	doc, err := goquery.NewDocument(url)
-	history := NewRateHistoryStruct(name)
-
-	if err != nil {
-		log.Print(err)
-	}
-	doc.Find("tbody").Find("tr").Each(func(i int, s *goquery.Selection) {
-		date := s.Find("td").Eq(0).Text()
-		date1, _ := time.Parse("2006/01/02", date)
-
-		cashBuy, _ := strconv.ParseFloat(s.Find("td").Eq(2).Text(), 64)
-		cashSell, _ := strconv.ParseFloat(s.Find("td").Eq(3).Text(), 64)
-		rateBuy, _ := strconv.ParseFloat(s.Find("td").Eq(4).Text(), 64)
-		rateSell, _ := strconv.ParseFloat(s.Find("td").Eq(5).Text(), 64)
-
-		history.CashBuy = append(history.CashBuy, cashBuy)
-		history.CashSell = append(history.CashSell, cashSell)
-		history.RateBuy = append(history.RateBuy, rateBuy)
-		history.RateSell = append(history.RateSell, rateSell)
-		history.Date = append(history.Date, date1)
-
-	})
-	return history
-}
-
-func createChart(data *RateHistoryStruct) *bytes.Buffer {
+func createChart(data *service.RateHistoryStruct) *bytes.Buffer {
 	graph := chart.Chart{
 		Title: data.CurrencyName + "(" + data.Date[0].Format("Jan 2 2006") + ")",
 		TitleStyle: chart.Style{
@@ -138,7 +93,7 @@ func createChart(data *RateHistoryStruct) *bytes.Buffer {
 	return buffer
 }
 
-func makeTicks(data *RateHistoryStruct) (ticks []chart.Tick) {
+func makeTicks(data *service.RateHistoryStruct) (ticks []chart.Tick) {
 	min, max := findSliceMinMax(data)
 	scale, interval, fixed := getTicksIntervalArgs(max)
 	dMax := decimal.NewFromFloat(max).Mul(decimal.NewFromFloat(scale)).Ceil().Div(decimal.NewFromFloat(scale))
@@ -167,7 +122,7 @@ func getTicksIntervalArgs(input float64) (scale, interval float64, fixed int32) 
 	return
 }
 
-func findSliceMinMax(v *RateHistoryStruct) (min, max float64) {
+func findSliceMinMax(v *service.RateHistoryStruct) (min, max float64) {
 	if len(v.CashSell) > 0 && len(v.CashBuy) > 0 {
 		min = v.CashBuy[0]
 		max = v.CashSell[0]
--- service/fetchHistoryRate.go
+++ service/fetchHistoryRate.go
@@ -1,37 +1,51 @@
 package service
 
-// type RateHistoryStruct struct {
-// 	CashBuy      []float64
-// 	CashSell     []float64
-// 	RateBuy      []float64
-// 	RateSell     []float64
-// 	Date         []time.Time
-// 	CurrencyName string
-// }
+import (
+	"log"
+	"strconv"
+	"time"
 
-// func getData(date, name string) *RateHistoryStruct {
-// 	url := "http://rate.bot.com.tw/xrt/quote/" + date + "/" + name + ""
-// 	doc, err := goquery.NewDocument(url)
-// 	history := NewRateHistoryStruct(name)
+	"github.com/PuerkitoBio/goquery"
+)
 
-// 	if err != nil {
-// 		log.Print(err)
-// 	}
-// 	doc.Find("tbody").Find("tr").Each(func(i int, s *goquery.Selection) {
-// 		date := s.Find("td").Eq(0).Text()
-// 		date1, _ := time.Parse("2006/01/02", date)
+type RateHistoryStruct struct {
+	CashBuy      []float64
+	CashSell     []float64
+	RateBuy      []float64
+	RateSell     []float64
+	Date         []time.Time
+	CurrencyName string
+}
 
-// 		cashBuy, _ := strconv.ParseFloat(s.Find("td").Eq(2).Text(), 64)
-// 		cashSell, _ := strconv.ParseFloat(s.Find("td").Eq(3).Text(), 64)
-// 		rateBuy, _ := strconv.ParseFloat(s.Find("td").Eq(4).Text(), 64)
-// 		rateSell, _ := strconv.ParseFloat(s.Find("td").Eq(5).Text(), 64)
+func NewRateHistoryStruct(name string) *RateHistoryStruct {
+	obj := new(RateHistoryStruct)
+	obj.CurrencyName = name
+	return obj
+}
 
-// 		history.CashBuy = append(history.CashBuy, cashBuy)
-// 		history.CashSell = append(history.CashSell, cashSell)
-// 		history.RateBuy = append(history.RateBuy, rateBuy)
-// 		history.RateSell = append(history.RateSell, rateSell)
-// 		history.Date = append(history.Date, date1)
+func GetData(date, name string) *RateHistoryStruct {
+	url := "http://rate.bot.com.tw/xrt/quote/" + date + "/" + name + ""
+	doc, err := goquery.NewDocument(url)
+	history := NewRateHistoryStruct(name)
 
-// 	})
-// 	return history
-// }
+	if err != nil {
+		log.Print(err)
+	}
+	doc.Find("tbody").Find("tr").Each(func(i int, s *goquery.Selection) {
+		date := s.Find("td").Eq(0).Text()
+		date1, _ := time.Parse("2006/01/02", date)
+
+		cashBuy, _ := strconv.ParseFloat(s.Find("td").Eq(2).Text(), 64)
+		cashSell, _ := strconv.ParseFloat(s.Find("td").Eq(3).Text(), 64)
+		rateBuy, _ := strconv.ParseFloat(s.Find("td").Eq(4).Text(), 64)
+		rateSell, _ := strconv.ParseFloat(s.Find("td").Eq(5).Text(), 64)
+
+		history.CashBuy = append(history.CashBuy, cashBuy)
+		history.CashSell = append(history.CashSell, cashSell)
+		history.RateBuy = append(history.RateBuy, rateBuy)
+		history.RateSell = append(history.RateSell, rateSell)
+		history.Date = append(history.Date, date1)
+
+	})
+	return history
+}
