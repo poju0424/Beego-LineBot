25bb6427580f7d3bb2ea27f4e48a9d191f4762e9
poju LEE <poju0424@gmail.com>
2017-06-26 23:48:48 +0800

    test push

--- routers/router.go
+++ routers/router.go
@@ -11,5 +11,6 @@ func init() {
 	beego.Handler("/currency/?:time/?:name", &controllers.CurrencyHandler{})
 
 	beego.Handler("/?:uuid/SensorData/?:sensorDataType/?:days", &controllers.SensorDataHandler{})
+	beego.Handler("/push", &controllers.PushHandler{})
 	// beego.Router("/123", &controllers.MainController{})
 }
